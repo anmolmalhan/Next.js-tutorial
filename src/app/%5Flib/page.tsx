9bc2a324f9be39696ce667b99ed555b557318bc2
Anmol Malhan <malhananmol1@gmail.com>
2026-03-27 01:17:26 +0530

    added leaning of private folder feature of next.js

---
+++ src/app/%5Flib/page.tsx
@@ -0,0 +1,9 @@
+export default function LibPage() {
+    return (
+        <div>
+            <h1>This will not be shown in the browser</h1>
+            <p>If we use %5F instead of _(underscore) it will be shown as is.<br></br>
+            and if we use underscore as a folder name then it will be treated as a private folder.</p>
+        </div>
+    );
+}
