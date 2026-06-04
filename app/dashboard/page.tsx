a7bfb440809a7f51bdad800db64ca3432b6a750f
Anmol Malhan <malhananmol1@gmail.com>
2026-06-04 17:57:59 +0530

    Added delay a response for demo purposes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data
 export default async function Page() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
-    const { } = await fetchCardData();
+    const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
